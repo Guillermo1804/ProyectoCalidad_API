6f18ff1fe1f64ffe4c0057976b7affe9386d0069
Guillermo1804 <memosanchez101@gmail.com>
2025-04-02 11:55:32 -0600

    Admin
    solo se creó el modelo basico del administrador para verificar que la bae de datos funciona correctamente

--- Calidad_API/models.py
+++ Calidad_API/models.py
@@ -5,3 +5,20 @@ from rest_framework.authentication import TokenAuthentication # type: ignore
 from django.contrib.auth.models import AbstractUser, User # type: ignore
 from django.conf import settings # type: ignore
 
+class BearerTokenAuthentication(TokenAuthentication):
+    keyword = u"Bearer"
+
+
+class Administradores(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    clave_admin = models.CharField(max_length=255,null=True, blank=True)
+    telefono = models.CharField(max_length=255, null=True, blank=True)
+    rfc = models.CharField(max_length=255,null=True, blank=True)
+    edad = models.IntegerField(null=True, blank=True)
+    ocupacion = models.CharField(max_length=255,null=True, blank=True)
+    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "Perfil del admin "+self.first_name+" "+self.last_name
